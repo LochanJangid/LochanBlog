22f91a73996107292af27b559370df854489778b
Lochan Jangid <129925009+LochanJangid@users.noreply.github.com>
2023-06-05 11:56:20 -0700

    📞

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     </Head>
     <div className="bg-gray-900 pb-6 sm:pb-8 lg:pb-12 flex justify-center item-center w-[100vw]">
     <section className="flex flex-col w-[80vw] items-center justify-center rounded-lg bg-gray-800 py-16 shadow-lg md:py-20 xl:py-48">
-        <p className="mb-4 text-center text-lg text-indigo-200 sm:text-xl md:mb-8">
+        <p className="mb-4 text-center text-lg text-indigo-200  sm:text-xl md:mb-8">
           Welcome to <b>Lochan Blog</b>
         </p>
         <h1 className="mb-8 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl">
